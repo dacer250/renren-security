8d920cec6ea2f25fc87776507676cdc239f7b874
ddj <dongdianjing@zyw18.com>
2021-07-24 22:11:56 +0800

    增加百度识别

--- renren-admin/src/main/resources/statics/js/modules/sys/baidures.js
+++ renren-admin/src/main/resources/statics/js/modules/sys/baidures.js
@@ -4,7 +4,12 @@ $(function () {
         datatype: "json",
         colModel: [			
 			{ label: 'id', name: 'id', index: 'id', width: 50, key: true },
-			{ label: '', name: 'fileId', index: 'file_id', width: 80 }, 			
+			{ label: '', name: 'fileId', index: 'file_id', width: 80,
+        cellattr: function(rowId, tv, rawObject, cm, rdata) {
+          //合并单元格
+          return 'id=\'fileId' + rowId + "\'";
+          //if (Number(rowId) < 5) { return ' colspan=2' }
+        }},
 			{ label: '', name: 'seq', index: 'seq', width: 80 }, 			
 			{ label: '', name: 'name', index: 'name', width: 80 }, 			
 			{ label: '', name: 'sex', index: 'sex', width: 80 }, 			
@@ -34,11 +39,36 @@ $(function () {
         },
         gridComplete:function(){
         	//隐藏grid底部滚动条
-        	$("#jqGrid").closest(".ui-jqgrid-bdiv").css({ "overflow-x" : "hidden" }); 
+        	$("#jqGrid").closest(".ui-jqgrid-bdiv").css({ "overflow-x" : "hidden" });
+          var gridName = "jqGrid";
+          Merger(gridName, 'fileId');
         }
     });
 });
-
+function Merger(gridName, CellName) {
+  //得到显示到界面的id集合
+  var mya = $("#" + gridName + "").getDataIDs();
+  //当前显示多少条
+  var length = mya.length;
+  for (var i = 0; i < length; i++) {
+    //从上到下获取一条信息
+    var before = $("#" + gridName + "").jqGrid('getRowData', mya[i]);
+    //定义合并行数
+    var rowSpanTaxCount = 1;
+    for (j = i + 1; j <= length; j++) {
+      //和上边的信息对比 如果值一样就合并行数+1 然后设置rowspan 让当前单元格隐藏
+      var end = $("#" + gridName + "").jqGrid('getRowData', mya[j]);
+      if (before[CellName] == end[CellName]) {
+        rowSpanTaxCount++;
+        $("#" + gridName + "").setCell(mya[j], CellName, '', { display: 'none' });
+      } else {
+        rowSpanTaxCount = 1;
+        break;
+      }
+      $("#" + CellName + "" + mya[i] + "").attr("rowspan", rowSpanTaxCount);
+    }
+  }
+}
 var vm = new Vue({
 	el:'#rrapp',
 	data:{
